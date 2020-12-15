7730080b117c4bb3fa3c715bf3f4bc9922d7aaff
Awet Tesfay <whitezscope@gmail.com>
2020-12-15 12:04:12 +0100

    post data and check it with the help of HTTP client tool in phpstorm

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const express = require('express')
+
 const app = express()
+app.use(express.json())
+
+const students = [{name:'awet', language:'php'}, {name:'feruz', language: 'javascript'}];
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
 })
 
-
-const student = [{name:'awet', language:'php'}, {name:'feruz', language: 'javascript'}];
-
 app.get('/students', (req, res)=> {
+    res.send(students);
+})
+
+app.get('/students/:name', (req, res)=> {
+    const name = req.params.name;
+    const student = students.find(s => s.name === name)
+    if (!student) res.status(404).send("student not found!")
     res.send(student);
+})
+
+
+app.post('/students', (req, res)=> {
+    const student = {
+        id: students.length + 1,
+        name: req.body.name
+    }
+    students.push(student);
+    res.send(students);
 })
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> {
     console.log(`listening to port number : ${port}`)
 })
+
+
